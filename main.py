c5cd65ff4564d61a149c4349bbef7c5c158ddc0f
rezazadehah <55617188+rezazadehah@users.noreply.github.com>
2021-07-02 23:54:59 +0430

    second push

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ colorama.init(autoreset=True)
 #background = BLUE
 
 
+def sky():
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
 def roof():
-    print(Back.LIGHTBLUE_EX + " " * 100)
-    print(Back.LIGHTBLUE_EX + " " * 100)
-    print(Back.LIGHTBLUE_EX + " " * 100)
-    print(Back.LIGHTBLUE_EX + " " * 100)
-    print(Back.LIGHTBLUE_EX + " " * 100)
-    print(Back.LIGHTBLUE_EX + " " * 100)
-
     print(Back.LIGHTBLUE_EX + " "*48 + Back.RED +" "*4 + Back.LIGHTBLUE_EX + " "*48)
     print(Back.LIGHTBLUE_EX + " "*45 + Back.RED +" "*10 + Back.LIGHTBLUE_EX + " "*45)
     print(Back.LIGHTBLUE_EX + " "*42 + Back.RED +" "*16 + Back.LIGHTBLUE_EX + " "*42)
@@ -23,27 +23,39 @@ def roof():
     print(Back.LIGHTBLUE_EX + " "*31 + Back.RED +" "*38 + Back.LIGHTBLUE_EX+" "*31)
     print(Back.LIGHTBLUE_EX + " "*30 + Back.RED +" "*40 + Back.LIGHTBLUE_EX+" "*30)
 
+def body():
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*24 +Back.YELLOW +" "*10 + Back.LIGHTMAGENTA_EX +" "*6+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*24 +Back.YELLOW +" "*10 + Back.LIGHTMAGENTA_EX +" "*6+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*24 +Back.YELLOW +" "*10 + Back.LIGHTMAGENTA_EX +" "*6+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*24 +Back.YELLOW +" "*10 + Back.LIGHTMAGENTA_EX +" "*6+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*6 +Back.LIGHTYELLOW_EX +" "*10 + Back.LIGHTMAGENTA_EX +" "*24+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*6 +Back.LIGHTYELLOW_EX +" "*10 + Back.LIGHTMAGENTA_EX +" "*24+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*6 +Back.LIGHTYELLOW_EX +" "*10 + Back.LIGHTMAGENTA_EX +" "*24+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*6 +Back.LIGHTYELLOW_EX +" "*10 + Back.LIGHTMAGENTA_EX +" "*24+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*6 +Back.LIGHTYELLOW_EX +" "*10 + Back.LIGHTMAGENTA_EX +" "*24+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*6 +Back.LIGHTYELLOW_EX +" "*10 + Back.LIGHTMAGENTA_EX +" "*24+ Back.LIGHTBLUE_EX+" "*30)
+    print(Back.GREEN + " "*30 + Back.LIGHTMAGENTA_EX +" "*6 +Back.LIGHTYELLOW_EX +" "*10 + Back.LIGHTMAGENTA_EX +" "*24+ Back.GREEN+" "*30)
 
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
-    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+def gnd():
+    print(Back.GREEN + " "*32 +Back.BLACK +" "*18 + Back.GREEN+" "*50)
+    print(Back.GREEN + " "*34 +Back.BLACK +" "*14 + Back.GREEN+" "*52)
+    print(Back.GREEN + " "*36 +Back.BLACK +" "*10 + Back.GREEN+" "*54)
+    print(Back.GREEN + " " * 100)
+    print(Back.GREEN + " " * 100)
+    print(Back.GREEN + " " * 100)
+    print(Back.GREEN + " " * 100)
+    print(Back.GREEN + " " * 100)
+    
 
-
-    print(Back.LIGHTBLUE_EX + " " * 100)
-    print(Back.LIGHTBLUE_EX + " " * 100)
-    print(Back.LIGHTBLUE_EX + " " * 100)
-    print(Back.LIGHTBLUE_EX + " " * 100)
-    print(Back.LIGHTBLUE_EX + " " * 100)
-
-
+sky()
 roof()
+body()
+gnd()
