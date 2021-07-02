31f0ff2e13725c2c9f58b567ab4c959f8ea71be0
rezazadehah <55617188+rezazadehah@users.noreply.github.com>
2021-07-02 23:26:41 +0430

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import colorama
+from colorama import Back, Style
+
+colorama.init(autoreset=True)
+
+#background = BLUE
+
+
+def roof():
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+
+    print(Back.LIGHTBLUE_EX + " "*48 + Back.RED +" "*4 + Back.LIGHTBLUE_EX + " "*48)
+    print(Back.LIGHTBLUE_EX + " "*45 + Back.RED +" "*10 + Back.LIGHTBLUE_EX + " "*45)
+    print(Back.LIGHTBLUE_EX + " "*42 + Back.RED +" "*16 + Back.LIGHTBLUE_EX + " "*42)
+    print(Back.LIGHTBLUE_EX + " "*40 + Back.RED +" "*20 + Back.LIGHTBLUE_EX + " "*40)
+    print(Back.LIGHTBLUE_EX + " "*37 + Back.RED +" "*26 + Back.LIGHTBLUE_EX+" "*37)
+    print(Back.LIGHTBLUE_EX + " "*34 + Back.RED +" "*32 + Back.LIGHTBLUE_EX+" "*34)
+    print(Back.LIGHTBLUE_EX + " "*31 + Back.RED +" "*38 + Back.LIGHTBLUE_EX+" "*31)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.RED +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+
+
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+    print(Back.LIGHTBLUE_EX + " "*30 + Back.LIGHTMAGENTA_EX +" "*40 + Back.LIGHTBLUE_EX+" "*30)
+
+
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+    print(Back.LIGHTBLUE_EX + " " * 100)
+
+
+roof()
